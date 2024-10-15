b59fc0381932872ab5cb58bfe4a77049f3acbb36
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-10-15 23:18:04 +0600

    Dark mood added in the Blog Details page

--- src/Pages/Blogs/components/BlogDetails.jsx
+++ src/Pages/Blogs/components/BlogDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import sanitize from "sanitize-html";
 
 const BlogDetails = () => {
     const blog = useLoaderData();
@@ -6,14 +7,28 @@ const BlogDetails = () => {
     const { title, image, content, posted_date, blogger } = blog;
 
     const newPostedDate = new Date(posted_date).toLocaleDateString('en-GB');
+    // console.log(content);
+
+    // Clean the default style of the content
+    const cleanContent = sanitize(content, {
+        allowedTags: ['b', 'i', 'em', 'strong', 'a', 'p', 'div','h3', 'img'],
+        allowedAttributes: {
+            '*': ['style', 'class'],
+            'a': ['href'],
+            'img': ['src', 'alt']
+        },
+        disallowedTagsMode: 'discard'
+    });
+
+    console.log(cleanContent)
 
     return (
-        <section className="bg-white dark:bg-gray-900 px-3 md:px-8 xl:px-24">
-            <div className="container py-16 mx-auto text-center">
+        <div className="bg-white dark:bg-[#292929] px-3 md:px-8 xl:px-24 py-3 md:py-6">
+            <div className="container mx-auto text-center">
                 <div className="w-full lg:max-w-xl mx-auto">
-                    <h1 className="text-2xl font-semibold text-gray-800 dark:text-white capitalize lg:text-4xl">{title}</h1>
+                    <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-100 capitalize lg:text-4xl">{title}</h1>
                     {/* <p className="mt-6 text-gray-500 dark:text-gray-300">Blood donation is a simple, safe, and impactful way to contribute to your community. By setting aside just a small amount of time, you can make a significant difference in {"someone's"} life.</p> */}
-                    <div className="flex flex-col md:flex-row justify-between">
+                    <div className="flex flex-col md:flex-row justify-between dark:text-gray-200">
                         <div className="flex flex-col items-center md:items-start mt-6">
                             <p><span className="font-semibold">Blogger:</span> {blogger?.name}</p>
                             <p><span className="font-semibold">Email:</span> {blogger?.email}</p>
@@ -26,17 +41,11 @@ const BlogDetails = () => {
                     <img className="object-cover w-full max-h-[40rem] rounded-xl lg:w-[40rem]" src={image} alt="Blog image" />
 
                     <div className="text-start">
-
-
-                        <p dangerouslySetInnerHTML={{ __html: content }}></p>
+                        <div dangerouslySetInnerHTML={{ __html: cleanContent }} className="blog-content-bg bg-red-500"></div>
                     </div>
                 </div>
-
-                <div className="flex flex-col justify-start border w-fit">
-
-                </div>
             </div>
-        </section>
+        </div>
     );
 };
 
